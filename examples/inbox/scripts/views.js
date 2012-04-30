5f9ea395f81c9b6f0bbd3c8cf4b488e32e9a0996
Paul Frazee <pfrazee@gmail.com>
2012-04-30 10:25:44 -0500

    how I want the templates to work

--- examples/inbox/scripts/views.js
+++ examples/inbox/scripts/views.js
@@ -1,47 +1,41 @@
 define(['link/tint', 'text!templates/layout.html', 'text!templates/inbox.html'], function(Tint, layoutHtml, inboxHtml) {
-    // Pre-compile templates
-    var LayoutTmpl = Tint.compile(layoutHtml);
-    var InboxTmpl = Tint.compile(inboxHtml);
     
-    // Views
-    var Views = {};
-    Views.layout = function(inboxService, content) {
-        var tmpl = new LayoutTmpl();
-        
-        // Static nav
-        tmpl.nav.item().header('Inbox');
-        tmpl.nav.item().link('Messages', 'inbox', inboxService.uri());
-        tmpl.nav.item().link('Settings', 'cog', inboxService.uri() + '/settings');
-        
-        // Services
-        tmpl.nav.item().header('Services');
-        for (var slug in inboxService.services) {
-            var service = inboxService.services[slug];
-            if (!service || !service.settings) { continue; }
-            tmpl.nav.item().link(service.settings.name, 'envelope', service.uri);
-        }
-        
-        // Content
-        tmpl.content = content;
-
-        return tmpl.toString();
+    // Layout
+    // ======
+    var LayoutView = Tint.compile(layoutHtml, function(baseUri, content) {
+        // Standard nav
+        this.nav.item().header('Inbox');
+        this.nav.item().link('Messages', 'inbox', baseUri;
+        this.nav.item().link('Settings', 'cog', baseUri + '/settings');
+        this.nav.item().header('Services');
+        // Set content
+        this.content = content;
+    });
+    LayoutView.addNavService = function(service) {
+        if (!service || !service.settings) { return; }
+        this.nav.item().link(service.settings.name, 'envelope', service.uri)
     };
-    Views.inbox = function(inbox, messages) {
+    
+    // Inbox
+    // =====
+    var InboxView = Tint.compile(inboxHtml, function(uri) {
+        this.inboxUri = uri;
+    });
+    InboxView.addMessages = function(messages) {
         if (!messages) { return ''; }
-        var tmpl = new InboxTmpl();
-        tmpl.inboxUri = inbox.uri();
-        
         // Sort by date
         messages.sort(function(a,b) { return ((a.date.getTime() < b.date.getTime()) ? 1 : -1); });
-        
         // Add to template
         for (var i=0; i < messages.length; i++) {
-            var message = messages[i];
-            tmpl.message(message.service, message.view_link, message.summary, new Date(message.date).toLocaleDateString() + ' @' + new Date(message.date).toLocaleTimeString());
-        }
-
-        return tmpl.toString();
+            this.addMessage(messages[i]);
+        }        
     };
+    InboxView.addMessage = function(message) {
+        this.message(message.service, message.view_link, message.summary, new Date(message.date).toLocaleDateString() + ' @' + new Date(message.date).toLocaleTimeString());
+    };
+    
+    // OLD
+    // ===
     Views.settings = function(content) {
         return content;
     };
@@ -49,6 +43,6 @@ define(['link/tint', 'text!templates/layout.html', 'text!templates/inbox.html'],
         return ['<div class="alert alert-error">', message, '</div>'].join('');
     };
 
-    // Export
+    // Export :TODO:
     return Views;
 });
